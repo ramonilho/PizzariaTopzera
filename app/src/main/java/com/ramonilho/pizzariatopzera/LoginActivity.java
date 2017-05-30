ed35ab32b400992bebc195cb8a8a11033307b1a4
Ramon Honório <ramonaqh@gmail.com>
2017-05-29 22:49:39 -0300

    login: fiap, senha: 123 || tela de pedido e cálculo

--- app/src/main/java/com/ramonilho/pizzariatopzera/LoginActivity.java
+++ app/src/main/java/com/ramonilho/pizzariatopzera/LoginActivity.java
@@ -1,13 +1,40 @@
 package com.ramonilho.pizzariatopzera;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText etLogin;
+    private EditText etSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etLogin = (EditText) findViewById(R.id.etLogin);
+        etSenha = (EditText) findViewById(R.id.etSenha);
     }
+
+    public void logar(View view){
+        String login = etLogin.getText().toString();
+        String senha = etSenha.getText().toString();
+
+        // Valida usuario e senha MOCK
+        if (login.equals("fiap") && senha.equals("123")) {
+            Intent intent = new Intent(this, OrderActivity.class);
+            intent.putExtra("user", login);
+
+            startActivity(intent);
+            finish();
+        } else {
+            Toast.makeText(this, "Usuário ou senha inválidos", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
